2867df22dc21b228e9f261b7e5dcb271d32a1ca1
andra <andrabaktiangjaya@gmail.com>
2025-08-20 22:19:38 +0800

    set up gradle and springboot

---
+++ src/test/java/com/demo/inventorysalesapp/InventorySalesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.inventorysalesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySalesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
